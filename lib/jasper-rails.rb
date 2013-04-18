8eabde0effd8105d4b7d1105772c836c099c215b
Venkat K <venkat@mahaswami.com>
2013-04-18 18:00:36 +0530

    1. Fixes to workaround Passenger Limitation 2. Default to inline disposition of file delivery

--- lib/jasper-rails.rb
+++ lib/jasper-rails.rb
@@ -35,54 +35,61 @@ module JasperRails
     attr_accessor :config
   end
 
-  classpath = '.'
-  Dir["#{File.dirname(__FILE__)}/java/*.jar"].each do |jar|
-    classpath << File::PATH_SEPARATOR + File.expand_path(jar)
-  end
-
-  Dir["lib/*.jar"].each do |jar|
-    classpath << File::PATH_SEPARATOR + File.expand_path(jar)
-  end
-
-  Rjb::load( classpath, ['-Djava.awt.headless=true','-Xms128M', '-Xmx256M'] )
-
-  Locale                      = Rjb::import 'java.util.Locale'
-  JRException                 = Rjb::import 'net.sf.jasperreports.engine.JRException'
-  JasperCompileManager        = Rjb::import 'net.sf.jasperreports.engine.JasperCompileManager'
-  JasperExportManager         = Rjb::import 'net.sf.jasperreports.engine.JasperExportManager'
-  JasperFillManager           = Rjb::import 'net.sf.jasperreports.engine.JasperFillManager'
-  JasperPrint                 = Rjb::import 'net.sf.jasperreports.engine.JasperPrint'
-  JRXmlUtils                  = Rjb::import 'net.sf.jasperreports.engine.util.JRXmlUtils'
-  JREmptyDataSource           = Rjb::import 'net.sf.jasperreports.engine.JREmptyDataSource'
-  # This is here to avoid the "already initialized constant QUERY_EXECUTER_FACTORY_PREFIX" warnings.
-  JRXPathQueryExecuterFactory = silence_warnings{Rjb::import 'net.sf.jasperreports.engine.query.JRXPathQueryExecuterFactory'}
-  InputSource                 = Rjb::import 'org.xml.sax.InputSource'
-  StringReader                = Rjb::import 'java.io.StringReader'
-  HashMap                     = Rjb::import 'java.util.HashMap'
-  ByteArrayInputStream        = Rjb::import 'java.io.ByteArrayInputStream'
-  JavaString                  = Rjb::import 'java.lang.String'
-  JFreeChart                  = Rjb::import 'org.jfree.chart.JFreeChart'
-
-  # Default report params
-  self.config = {
-    :report_params=>{
-      "REPORT_LOCALE"    => Locale.new('en', 'US'),
-      "XML_LOCALE"       => Locale.new('en', 'US'),
-      "XML_DATE_PATTERN" => 'yyyy-MM-dd'
-    }
-  }
-
   module Jasper
     module Rails
+    
+      def self.classpath
+        classpaths = '.'
+        Dir["#{File.dirname(__FILE__)}/java/*.jar"].each do |jar|
+          classpaths << File::PATH_SEPARATOR + File.expand_path(jar)
+        end
+
+        Dir["lib/*.jar"].each do |jar|
+          classpaths << File::PATH_SEPARATOR + File.expand_path(jar)
+        end
+        classpaths
+      end
+      
       def self.render_pdf(jasper_file, datasource, parameters, options)
+      
+        Rjb::load( classpath, ['-Djava.awt.headless=true','-Xms128M', '-Xmx256M'] ) unless Rjb::loaded?
+      
+        # The code below is to workaround declaring constants within methods
+        # We would like to delay these till a request is received to workaround Apache Passenger issue
+        locale                      = Rjb::import 'java.util.Locale'
+        jRException                 = Rjb::import 'net.sf.jasperreports.engine.JRException'
+        jasperCompileManager        = Rjb::import 'net.sf.jasperreports.engine.JasperCompileManager'
+        jasperExportManager         = Rjb::import 'net.sf.jasperreports.engine.JasperExportManager'
+        jasperFillManager           = Rjb::import 'net.sf.jasperreports.engine.JasperFillManager'
+        jasperPrint                 = Rjb::import 'net.sf.jasperreports.engine.JasperPrint'
+        jRXmlUtils                  = Rjb::import 'net.sf.jasperreports.engine.util.JRXmlUtils'
+        jREmptyDataSource           = Rjb::import 'net.sf.jasperreports.engine.JREmptyDataSource'
+        jRXPathQueryExecuterFactory = silence_warnings{Rjb::import 'net.sf.jasperreports.engine.query.JRXPathQueryExecuterFactory'}
+        inputSource                 = Rjb::import 'org.xml.sax.InputSource'
+        stringReader                = Rjb::import 'java.io.StringReader'
+        hashMap                     = Rjb::import 'java.util.HashMap'
+        byteArrayInputStream        = Rjb::import 'java.io.ByteArrayInputStream'
+        javaString                  = Rjb::import 'java.lang.String'
+        jFreeChart                  = Rjb::import 'org.jfree.chart.JFreeChart'
+      
         options ||= {}
         parameters ||= {}
         jrxml_file  = jasper_file.sub(/\.jasper$/, ".jrxml")
 
         begin
+        
+          # Default report params
+          config = {
+            :report_params=>{
+              "REPORT_LOCALE"    => locale.new('en', 'US'),
+              "XML_LOCALE"       => locale.new('en', 'US'),
+              "XML_DATE_PATTERN" => 'yyyy-MM-dd'
+            }
+          }
+        
           # Converting default report params to java HashMap
-          jasper_params = HashMap.new
-          JasperRails.config[:report_params].each do |k,v|
+          jasper_params = hashMap.new
+          config[:report_params].each do |k,v|
             jasper_params.put(k, v)
           end
 
@@ -90,31 +97,31 @@ module JasperRails
           # default when they already represent a JRB entity.
           # Pay attention that, for now, all other parameters are converted to string!
           parameters.each do |key, value|
-            jasper_params.put(JavaString.new(key.to_s), parameter_value_of(value))
+            jasper_params.put(javaString.new(key.to_s), parameter_value_of(value))
           end
 
           # Compile it, if needed
           if !File.exist?(jasper_file) || (File.exist?(jrxml_file) && File.mtime(jrxml_file) > File.mtime(jasper_file))
-            JasperCompileManager.compileReportToFile(jrxml_file, jasper_file)
+            jasperCompileManager.compileReportToFile(jrxml_file, jasper_file)
           end
 
           # Fill the report
           if datasource
-            input_source = InputSource.new
-            input_source.setCharacterStream(StringReader.new(datasource.to_xml(options).to_s))
+            input_source = inputSource.new
+            input_source.setCharacterStream(stringReader.new(datasource.to_xml(options).to_s))
             data_document = silence_warnings do
               # This is here to avoid the "already initialized constant DOCUMENT_POSITION_*" warnings.
-              JRXmlUtils._invoke('parse', 'Lorg.xml.sax.InputSource;', input_source)
+              jRXmlUtils._invoke('parse', 'Lorg.xml.sax.InputSource;', input_source)
             end
 
-            jasper_params.put(JRXPathQueryExecuterFactory.PARAMETER_XML_DATA_DOCUMENT, data_document)
-            jasper_print = JasperFillManager.fillReport(jasper_file, jasper_params)
+            jasper_params.put(jRXPathQueryExecuterFactory.PARAMETER_XML_DATA_DOCUMENT, data_document)
+            jasper_print = jasperFillManager.fillReport(jasper_file, jasper_params)
           else
-            jasper_print = JasperFillManager.fillReport(jasper_file, jasper_params, JREmptyDataSource.new)
+            jasper_print = jasperFillManager.fillReport(jasper_file, jasper_params, jREmptyDataSource.new)
           end
 
           # Export it!
-          JasperExportManager._invoke('exportReportToPdf', 'Lnet.sf.jasperreports.engine.JasperPrint;', jasper_print)
+          jasperExportManager._invoke('exportReportToPdf', 'Lnet.sf.jasperreports.engine.JasperPrint;', jasper_print)
         rescue Exception=>e
           if e.respond_to? 'printStackTrace'
             ::Rails.logger.error e.message
@@ -129,12 +136,13 @@ module JasperRails
       # Returns the value without conversion when it's converted to Java Types.
       # When isn't a Rjb class, returns a Java String of it.
       def self.parameter_value_of(param)
+        javaString                  = Rjb::import 'java.lang.String'
         # Using Rjb::import('java.util.HashMap').new, it returns an instance of
         # Rjb::Rjb_JavaProxy, so the Rjb_JavaProxy parent is the Rjb module itself.
         if param.class.parent == Rjb
           param
         else
-          JavaString.new(param.to_s)
+          javaString.new(param.to_s)
         end
       end
     end
@@ -142,14 +150,16 @@ module JasperRails
 
   class ActionController::Responder
     def to_pdf
-      jasper_file = "#{Rails.root.to_s}/app/views/#{controller.controller_path}/#{controller.action_name}.jasper"
+      jasper_file =  (@options[:template] && "#{Rails.root.to_s}/app/views/#{controller.controller_path}/#{@options[:template]}") ||
+          "#{Rails.root.to_s}/app/views/#{controller.controller_path}/#{controller.action_name}.jasper"
 
       params = {}
       controller.instance_variables.each do |v|
         params[v.to_s[1..-1]] = controller.instance_variable_get(v)
       end
 
-      controller.send_data Jasper::Rails::render_pdf(jasper_file, resource, params, options), :type => Mime::PDF
+      controller.send_data Jasper::Rails::render_pdf(jasper_file, resource, params, options), :type => Mime::PDF, 
+            :disposition => 'inline'
     end
   end
 
